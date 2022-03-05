138864b24fd075bbd49d6d461fd429442e42ef67
deanwallflower <dean@deanwallflower.com>
2022-03-05 17:54:16 +0100

    event listeners for swipe, swipe up to show more info

--- main.js
+++ main.js
@@ -1,5 +1,64 @@
+// SWIPE UP GESTURE DETECTION
+let touchstartY = 0
+let touchendY = 0
+let touchstartX = 0
+let touchendX = 0
+
+const bg = document.getElementById('bg')
+var currentInfoBoxId = "info-1";
+
+function swipeVertical() {
+  if (Math.abs(touchendX - touchstartX) > 20) return;
+  if (touchendY < touchstartY) swipeUp();
+  if (touchendY > touchstartY) swipeDown();
+  }
+
+bg.addEventListener('touchstart', e => {
+  touchstartY = e.changedTouches[0].screenY;
+  touchstartX = e.changedTouches[0].screenX;
+})
+
+bg.addEventListener('touchend', e => {
+  touchendY = e.changedTouches[0].screenY;
+  touchendX = e.changedTouches[0].screenX;
+  swipeVertical()
+})
+
+function swipeHorizontal() {
+  if (Math.abs(touchendY - touchstartY) > 20) return;
+  if (touchendX < touchstartX) setTimeout(swipeLeft, 700);
+  if (touchendX > touchstartX) setTimeout(swipeRight, 700);
+  }
+
+bg.addEventListener('touchstart', e => {
+  touchstartY = e.changedTouches[0].screenY;
+  touchstartX = e.changedTouches[0].screenX;
+})
+
+bg.addEventListener('touchend', e => {
+  touchendY = e.changedTouches[0].screenY;
+  touchendX = e.changedTouches[0].screenX;
+  swipeHorizontal()
+})
+
+function swipeUp() {
+  document.getElementById(currentInfoBoxId).classList.remove("translate-y-[100vh]")
+}
+function swipeDown() {
+  document.getElementById(currentInfoBoxId).classList.add("translate-y-[100vh]")
+}
+function swipeLeft() {
+  scrollX = bg.scrollLeft
+  currentInfoBoxId = `info-${Math.floor(scrollX / window.innerWidth + 1)}`
+  console.log(currentInfoBoxId)
+}
+function swipeRight() {
+  scrollX = bg.scrollLeft
+  currentInfoBoxId = `info-${Math.floor(scrollX / window.innerWidth + 1)}`
+  console.log(currentInfoBoxId)
+}
+
 // ABOUT TEXT DROPDOWN //
-// const dwButton = document.getElementById("dw-button")
 const header = document.getElementById("header")
 // const homeButton = document.getElementById("home")
 const aboutButton = document.getElementById("about")
@@ -10,14 +69,49 @@ const contactButton = document.getElementById("contact")
 const contactDropdown = document.getElementById("contact-form")
 // const navBar = document.getElementById("navbar")
 
+function selected () {
+  aboutButton.classList.toggle("text-shadow");
+  contactButton.classList.remove("text-shadow");
+}
+
+function showAboutDropdown () {
+  aboutTextDropdown.classList.toggle("h-0");
+  contactDropdown.classList.add("h-0")
+  contactButton.classList.remove("text-shadow");
+  header.classList.remove("h-0")
+  header.classList.remove("overflow-hidden")
+  document.getElementById(currentInfoBoxId).classList.add("translate-y-[100vh]")
+
+  
+}
+aboutButton.addEventListener("click", selected);
+aboutButton.addEventListener("click", showAboutDropdown);
+
+// CONTACT FORM DROPDOWN //
+
+function selected2 () {
+  contactButton.classList.toggle("text-shadow");
+  aboutButton.classList.remove("text-shadow");
+}
+
+function showContactDropdown () {
+  contactDropdown.classList.toggle("h-0");
+  aboutTextDropdown.classList.remove("text-shadow");
+  aboutTextDropdown.classList.add("h-0");
+  header.classList.remove("h-0")
+  header.classList.remove("overflow-hidden")
+  document.getElementById(currentInfoBoxId).classList.add("translate-y-[100vh]")
+}
+
+contactButton.addEventListener("click", selected2);
+contactButton.addEventListener("click", showContactDropdown);
+
+
 // HIDE HEADER ON FORM ENTRY
 const userName = document.getElementById("userName")
 const userEmail = document.getElementById("userEmail")
 const userMessage = document.getElementById("userMessage")
 
-const userNameClassList = Array.from(userName.classList)
-const userEmailClassList = Array.from(userEmail.classList)
-const userMessageClassList = Array.from(userMessage.classList)
 
 function hideHeader () {
   header.classList.add("h-0")
@@ -27,27 +121,6 @@ function hideHeader () {
 userName.addEventListener("focusin", hideHeader);
 userEmail.addEventListener("focusin", hideHeader);
 userMessage.addEventListener("focusin", hideHeader);
-
-function selected () {
-  aboutButton.classList.toggle("text-shadow");
-  // contactButton.classList.remove("text-shadow");
-  // dwButton.classList.toggle("selected");
-}
-
-function showAboutDropdown () {
-  console.log(userNameClassList)
-  aboutTextDropdown.classList.toggle("h-0");
-  // dwButton.classList.toggle("dw-button-dropdown");
-  // if(contactDropdownClassList.includes("h-0")) { 
-  // } else {
-  //   contactDropdown.classList.add("h-0");
-  //   contactDropdownClassList.push("h-0");
-  // }
-  contactDropdown.classList.add("h-0")
-  // navBar.classList.remove("navbar-dropdown");
-  contactButton.classList.remove("text-shadow");
-  
-}
 
 // function goHome () {
 //   aboutTextDropdown.classList.remove("about-text-dropdown");
@@ -62,56 +135,23 @@ function showAboutDropdown () {
   
 // }
 
-// dwButton.addEventListener("click", selected);
-// dwButton.addEventListener("click", showAboutDropdown);
-aboutButton.addEventListener("click", selected);
-aboutButton.addEventListener("click", showAboutDropdown);
 // homeButton.addEventListener("click", goHome);
 
-// CONTACT FORM DROPDOWN //
-
-
-function selected2 () {
-  contactButton.classList.toggle("text-shadow");
-  // aboutButton.classList.remove("text-shadow");
-}
-
-function showContactDropdown () {
-  contactDropdown.classList.toggle("h-0");
-  // navBar.classList.toggle("navbar-dropdown");
-  // aboutTextDropdown.classList.toggle("h-0");
-  // dwButton.classList.remove("dw-button-dropdown");
-  // dwButton.classList.remove("selected");
-  aboutTextDropdown.classList.remove("text-shadow");
-  aboutTextDropdown.classList.add("h-0");
-  header.classList.remove("h-0")
-  header.classList.remove("overflow-hidden")
-  // if(aboutTextClassList.includes("h-0")) { 
-    
-  // } else {
-  //   aboutTextDropdown.classList.add("h-0");
-  //   aboutTextClassList.push("h-0");
-  // }
-}
-
-contactButton.addEventListener("click", selected2);
-contactButton.addEventListener("click", showContactDropdown);
-
 // REEL NAVIGATION //
-const reelNav = document.getElementById("reel")
-const reelLeftButton = document.getElementById("left-carat")
-const reelRightButton = document.getElementById("right-carat")
+// const reelNav = document.getElementById("reel")
+// const reelLeftButton = document.getElementById("left-carat")
+// const reelRightButton = document.getElementById("right-carat")
 
-function selected3 () {
-  reelNav.classList.toggle("selected");
-}
-function showReelButtons () {
-  reelLeftButton.classList.toggle("carat-popup");
-  reelRightButton.classList.toggle("carat-popup");
-}
+// function selected3 () {
+//   reelNav.classList.toggle("selected");
+// }
+// function showReelButtons () {
+//   reelLeftButton.classList.toggle("carat-popup");
+//   reelRightButton.classList.toggle("carat-popup");
+// }
 
-reelNav.addEventListener("click", selected3);
-reelNav.addEventListener("click", showReelButtons);
+// reelNav.addEventListener("click", selected3);
+// reelNav.addEventListener("click", showReelButtons);
 
 // CLICK OUTSIDE TO RETURN //
 // window.onclick = function(event) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  mode: 'jit',
   content: [
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
@@ -6,7 +7,19 @@ module.exports = {
     fontFamily: {
       'robo': ['roboto mono', 'regular', 'semi-bold']
     },
-    extend: {},
+    extend: {
+      boxShadow: {
+        'md': '0 0.1rem 0.2rem rgba(0, 0, 0, 0.16), 0 0.2rem 0.4rem rgba(0, 0, 0, 0.12), 0 0.1rem 0.8rem rgba(0, 0, 0, 0.1)',
+        'inner': 'inset 0.4rem 0.4rem 1rem 0 rgba(255, 0, 0, 0.25), inset -0.4rem -0.4rem 1rem 0 rgba(255, 0, 0, 0.25)',
+      },
+    },
   },
   plugins: [],
 }
+
+
+// // .box-shadow: {
+//   0px 1px 2px rgba(0, 0, 0, 0.16), 
+//   0px 2px 4px rgba(0, 0, 0, 0.12), 
+//   0px 1px 8px rgba(0, 0, 0, 0.1)
+// }
